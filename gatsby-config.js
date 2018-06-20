139021af82edfb782cc63a1935722fcaea9e7b7a
lucaslos <lucas@sugestly.com>
2018-06-20 04:33:54 -0300

    Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,33 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${__dirname}/src/images`,
         name: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Trad Dalberto Arquitetura',
+        short_name: 'Trad Dalberto',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#42515A',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        navigateFallback: null,
+        navigateFallbackWhitelist: [],
       },
     },
     'gatsby-transformer-remark',
