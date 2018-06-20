6144609a94a749a246e2133c768a1622c907a2b6
lucaslos <lucas@sugestly.com>
2018-06-20 05:02:33 -0300

    remove netlify-cms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,6 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-react-next',
     'gatsby-plugin-netlify',
-    'gatsby-plugin-netlify-cms',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
   ],
