a00ff957627bcdd572e014028caf62e9b4ead96a
lucaslos <lucas@sugestly.com>
2018-06-25 02:23:37 -0300

    Add google analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+        trackingId: 'UA-121334345-1',
       },
     },
     {
