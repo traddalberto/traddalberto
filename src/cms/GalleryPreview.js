b3bc0ae7e82524aadc0b17261fcdf12bc3e11dc1
lucaslos <lucas@sugestly.com>
2018-07-03 17:42:11 -0300

    Fix bug in photos preview

--- src/cms/GalleryPreview.js
+++ src/cms/GalleryPreview.js
@@ -6,24 +6,26 @@ const GalleryPreview = ({ entry, widgetsFor, getAsset }) => (
     <p>{entry.getIn(['data', 'description'])}</p>
 
     <h2>Imagens da galeria</h2>
-    {widgetsFor('gallery').map((item, i) => (
-      item.getIn(['data', 'img'])
-        ? <div key={i} className="gallery-item">
-          <img className={item.getIn(['data', 'featured']) ? 'featured' : ''} alt="" src={getAsset(item.getIn(['data', 'img']))} />
-          <p>{item.getIn(['data', 'description'])}</p>
-        </div>
-        : null
-    ))}
+    {widgetsFor('gallery').map((item, i) =>
+      item && item.getIn(['data', 'img'])
+        ? (
+          <div key={i} className="gallery-item">
+            <img className={item.getIn(['data', 'featured']) ? 'featured' : ''} alt="" src={getAsset(item.getIn(['data', 'img']))} />
+            <p>{item.getIn(['data', 'description'])}</p>
+          </div>
+        ) : null
+    )}
 
     <h2>Imagens do projeto</h2>
-    {widgetsFor('project').map((item, i) => (
-      item.getIn(['data', 'img'])
-        ? <div key={i} className="gallery-item">
-          <img className={item.getIn(['data', 'featured']) ? 'featured' : ''} alt="" src={getAsset(item.getIn(['data', 'img']))} />
-          <p>{item.getIn(['data', 'description'])}</p>
-        </div>
-        : null
-    ))}
+    {widgetsFor('project').map((item, i) =>
+      item && item.getIn(['data', 'img'])
+        ? (
+          <div key={i} className="gallery-item">
+            <img className={item.getIn(['data', 'featured']) ? 'featured' : ''} alt="" src={getAsset(item.getIn(['data', 'img']))} />
+            <p>{item.getIn(['data', 'description'])}</p>
+          </div>
+        ) : null
+    )}
   </div>
 );
 
