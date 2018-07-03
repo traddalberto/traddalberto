6d09aa3fa75f97f4613e623faef6651c84b86d42
lucaslos <lucas@sugestly.com>
2018-07-02 21:46:18 -0300

    Fix gallery feature field not defined

--- gatsby-node.js
+++ gatsby-node.js
@@ -54,7 +54,7 @@ exports.onCreateNode = ({
           imgRef: item.img,
           relPath,
           description: item.description || description || '',
-          featured: item.featured,
+          featured: item.featured || false,
         }
       });
 
