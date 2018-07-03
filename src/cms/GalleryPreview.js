9a6f442c93b85a79ba4cc0baab100d1b4b266423
lucaslos <lucas@sugestly.com>
2018-07-03 17:26:09 -0300

    Fix bug

--- src/cms/GalleryPreview.js
+++ src/cms/GalleryPreview.js
@@ -7,20 +7,22 @@ const GalleryPreview = ({ entry, widgetsFor, getAsset }) => (
 
     <h2>Imagens da galeria</h2>
     {widgetsFor('gallery').map((item, i) => (
-      item &&
-        <div key={i} className="gallery-item">
+      item
+        ? <div key={i} className="gallery-item">
           <img className={item.getIn(['data', 'featured']) ? 'featured' : ''} alt="" src={getAsset(item.getIn(['data', 'img']))} />
           <p>{item.getIn(['data', 'description'])}</p>
         </div>
+        : null
     ))}
 
     <h2>Imagens do projeto</h2>
     {widgetsFor('project').map((item, i) => (
-      item &&
-        <div key={i} className="gallery-item">
+      item
+        ? <div key={i} className="gallery-item">
           <img className={item.getIn(['data', 'featured']) ? 'featured' : ''} alt="" src={getAsset(item.getIn(['data', 'img']))} />
           <p>{item.getIn(['data', 'description'])}</p>
         </div>
+        : null
     ))}
   </div>
 );
